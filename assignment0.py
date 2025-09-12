742446fb0be065f263784444a76dd79df16258f5
LoganSnyder3 <snyderl27@chcstudent.com>
2025-09-12 05:23:35 -0400

    Create assignment0.py

---
+++ assignment0.py
@@ -0,0 +1,10 @@
+#Step 1
+x = int(input("Enter a number for x: "))
+y = int(input("Enter a number for y: "))
+
+#Step 2
+z = x * y
+
+#Step 3
+print(f"The product of {x} and {y} is {z}")
+input("Press Enter to Exit")
