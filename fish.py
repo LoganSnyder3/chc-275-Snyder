81f177c8c12718ecee948a52e730386f70abb554
danielkim321 <snyderl27@chcstudent.com>
2025-09-23 11:46:32 -0400

    Update fish.py

--- fish.py
+++ fish.py
@@ -1,7 +1,16 @@
 #Problem 2: fish.py
-print("Enter your fish number")
+print("Describe your fish:")
 print("1. Carnivorous")
 print("2. Salt Water")
 print("3. Community")
 option = input("Enter your fish number: ")
-if option 
+if option == "1":
+  option = input("Do you already have it? ")
+if (option == "yes"):
+    print("Too bad")
+elif (option == "no"):
+    print("Enjoy")
+elif option == "2":
+    option = print("You're a fancy fish parent")
+    
+
