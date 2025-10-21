140653836a581784ca2723fb7a08663037bb4fad
danielkim321 <dkthor2008@gmail.com>
2025-10-21 12:29:07 -0400

    Update mushrooms.py

--- mushrooms.py
+++ mushrooms.py
@@ -1,9 +1,27 @@
 #Problem 2 (mushrooms.py)
 
-print("Enter the size of your muschrooms")
-print("1. Enter the size of your mushroom")
-print("2. Type 'Stop' to quit")
-option = input("Enter the size of your mushroom: ")
-if option.isnumeric():
+mushrooms = []
+check = False
+while check == False:
+    print("Welcome to the mushroom program")
+    print("1. Enter the size of your mushroom")
+    print("2. Sort mushrooms into groups")
+    print("3. quit")
+    option = input("Enter your option: ")
+    if option == "1":
+        option = input("Enter the size of your mushroom: ")
+        if option.isnumeric():
+            option = int(option)
+            if option >= 100:          
+        else:
+            print(f"{option} was not a number")        
+    elif option == "2":
+        
+    elif option == "3":
+       print("Goodbye")
+       check = True
+    else:
+        print("Not a valid option")
+  
     
     
