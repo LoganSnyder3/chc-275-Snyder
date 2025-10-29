bac44b142b3417643474e2ef9da3c48fc22b8dcf
danielkim321 <dkthor2008@gmail.com>
2025-10-29 12:29:51 -0400

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,21 @@
+print("Welcome to my 4-function calculator")
+print("Which function to execute?")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+option = input("Enter which function to execute: ")
+if option == "1":
+    x = int(input("Enter Num 1: "))
+    y = int(input("Enter Num 2: "))
+    sum = x + y
+    print(sum)
+if option == "2":
+    x = int(input("Enter Num 1: "))
+    y = int(input("Enter Num 2: "))
+    difference = x - y
+    print(difference)
+    
+    
+
+
