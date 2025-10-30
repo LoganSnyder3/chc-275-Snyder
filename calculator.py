5ed4c813295f299be6890420b41b1fb8eb8c6403
danielkim321 <dkthor2008@gmail.com>
2025-10-30 11:22:21 -0400

    .

--- calculator.py
+++ calculator.py
@@ -1,20 +1,77 @@
-print("Welcome to my 4-function calculator")
-print("Which function to execute?")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
-option = input("Enter which function to execute: ")
-if option == "1":
-    x = int(input("Enter Num 1: "))
-    y = int(input("Enter Num 2: "))
-    sum = x + y
-    print(sum)
-if option == "2":
-    x = int(input("Enter Num 1: "))
-    y = int(input("Enter Num 2: "))
-    difference = x - y
-    print(difference)
+check = False
+while check == False:
+    print("Welcome to my 4-function calculator")
+    print("Which function to execute?")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Quit")
+    option = input("Enter which function to execute: ")
+    if option.strip() == "1":
+        try:
+            x = int(input("Enter Num 1: "))
+            y = int(input("Enter Num 2: "))
+            sum = x + y
+            print(sum)
+        except ValueError: 
+            print("x and y must be numbers")
+        except TypeError:
+            print("x and y must be integers")
+        except Exception as e:
+            print(e)
+        finally:
+            print("Thanks for using my calculator")
+    elif option.strip() == "2":
+        try:
+            x = int(input("Enter Num 1: "))
+            y = int(input("Enter Num 2: "))
+            difference = x - y
+            print(difference)
+        except ValueError: 
+            print("x and y must be numbers")
+        except TypeError:
+            print("x and y must be integers")
+        except Exception as e:
+            print(e)
+        finally:
+            print("Thanks for using my calculator")
+    elif option.strip() == "3":
+        try:
+            x = int(input("Enter Num 1: "))
+            y = int(input("Enter Num 2: "))
+            product = x * y
+            print(product)
+        except ValueError: 
+            print("x and y must be numbers")
+        except TypeError:
+            print("x and y must be integers")
+        except Exception as e:
+            print(e)
+        finally:
+            print("Thanks for using my calculator")
+    elif option.strip() =="4":
+        try:
+            x = int(input("Enter Num 1: "))
+            y = int(input("Enter Num 2: "))
+            quotient = x / y
+            print(quotient)
+        except ValueError: 
+            print("x and y must be numbers")
+        except ZeroDivisionError:
+            print("y must be nonzero")
+        except TypeError:
+            print("x and y must be integers")
+        except Exception as e:
+            print(e)
+        finally:
+            print("Thanks for using my calculator")
+    elif option.strip() == "5":
+        print("Goodbye")
+        check = True
+    else:
+        print("Invalid option")
+
     
     
 
