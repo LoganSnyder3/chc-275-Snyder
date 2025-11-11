813ab5c332ddecbc61fc807b80e7f32c6e50c2ca
danielkim321 <dkthor2008@gmail.com>
2025-11-11 14:43:36 -0500

    wdym

--- stockmarket.py
+++ stockmarket.py
@@ -1,7 +1,24 @@
-file = open("days1_20.txt","r")
-buffer = file.readlines()
-file.close()
+#Lab 3 - Stock Market Evaluation
 
-#msft
-msft = buffer[0].split()
-print(buffer[0])
+try:
+    file1 = open("days1_20.txt", "r")
+    buffer1 = file1.readlines()
+    file1.close()
+    
+    file2 = open("days21_40.txt", "r")
+    buffer2 = file2.readlines()
+    file2.close()
+
+except FileNotFoundError:
+    print("One or both files not found")
+else:
+    data1 = {}
+    data2 = {}
+
+    for line in buffer1:
+        parts = line.strip().split()
+        if len(parts) > 1:
+            ticker = parts[0]
+            prices = []
+            for value in parts[1]:
+               
