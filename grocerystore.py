8890027d5943b7d66035247fff720ba623c04875
danielkim321 <dkthor2008@gmail.com>
2025-11-18 11:44:12 -0500

    Create grocerystore.py

---
+++ grocerystore.py
@@ -0,0 +1,11 @@
+#Lab 4 - Grocery Store
+
+#Menu
+print("Welcome to the grocery store")
+print("1. Add to cart")
+print("2. Remove items")
+print("3. Check out")
+option = input("Enter your selection: ")
+if option == "1":
+    
+    
